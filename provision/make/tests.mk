e70e36a82a3c345c40d99dfb1c51321f5db9fa46
@slovacus <slovacus@gmail.com>
2019-12-10 13:50:32 -0500

    (#7) refactor: change test molecule

--- provision/make/tests.mk
+++ provision/make/tests.mk
@@ -14,7 +14,7 @@ test:
 
 test.all:
 	@echo $(MESSAGE) Running tests on the current Python interpreter with coverage $(END)
-	$(docker-test-run) bash -c "zunit --verbose"
+	$(PIPENV_RUN) molecule test
 
 test.lint:
 	$(PIPENV_RUN) pre-commit run --all-files --verbose
