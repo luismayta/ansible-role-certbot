eea7cdff2fb6c1f84e8f0b30482d38b7030bda82
@slovacus <slovacus@gmail.com>
2019-12-10 15:42:38 -0500

    (#7) test: change assert to assert_that

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -2,15 +2,19 @@
 import os
 
 import testinfra.utils.ansible_runner
+from hamcrest import assert_that, equal_to
+
+
+USERNAME: str = "root"
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ["MOLECULE_INVENTORY_FILE"]
 ).get_hosts("all")
 
 
-def test_hosts_file(host):
+def test_hosts_file(host) -> None:
     filename = host.file("/etc/hosts")
 
-    assert filename.exists
-    assert filename.user == "root"
-    assert filename.group == "root"
+    assert_that(filename.exists, equal_to(True))
+    assert_that(filename.host, equal_to("root"))
+    assert_that(filename.group, equal_to("root"))
