16786fc69f27b5afed0002aa58b618c2260d92dc
@slovacus <slovacus@gmail.com>
2019-12-11 07:30:56 -0500

    (#7) refactor: implement test

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,20 +1,25 @@
 # -*- coding: utf-8 -*-
 import os
 
+import pytest
 import testinfra.utils.ansible_runner
 from hamcrest import assert_that, equal_to
 
-
-USERNAME: str = "root"
-
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ["MOLECULE_INVENTORY_FILE"]
+    os.environ["MOLECULE_INVENTORY_FILE"],
 ).get_hosts("all")
 
 
-def test_hosts_file(host) -> None:
-    filename = host.file("/etc/hosts")
+def test_hosts_file(host):
+    exe = host.file("/etc/hosts")
+    assert_that(exe.exists, equal_to(True))
+    assert_that(exe.user, equal_to("root"))
+    assert_that(exe.group, equal_to("root"))
 
-    assert_that(filename.exists, equal_to(True))
-    assert_that(filename.host, equal_to("root"))
-    assert_that(filename.group, equal_to("root"))
+
+@pytest.mark.parametrize(
+    "name", [("python"), ("curl"), ("git"), ("gcc"),],
+)
+def test_build_dependencies(host, name):
+    pkg = host.package(name)
+    assert_that(pkg.is_installed, equal_to(True))
